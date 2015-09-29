cdf9320e46f638c761269bff3cce3162547aaef5
samuel sachin <samuelsachin@mapofmedicine.com>
2015-09-29 16:17:37 +0100

    g

--- manifests/install.pp
+++
@@ -1,6 +0,0 @@
-class sachinpuppet::install {
-  include sachinpuppet::params
-  package { [$::sachinpuppet::params::apache_package_name, $sachinpuppet::params::apacheutils_package_name, $sachinpuppet::params::logrotate_package_name]:
-    ensure => present,
-  }
-}
